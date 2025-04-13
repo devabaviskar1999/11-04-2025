d56bb93d1905e725178d4d74718ed8949d904443
devendra baviskar <devbaviskar1999@gmail.com>
2025-04-13 17:00:36 +0530

    something

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.route.js";
+import messageRoute from "./routes/message.route.js";
 import { MongoConnection } from "./lib/db.js";
 
 //!express calling
@@ -17,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //!Routing
 app.use("/api/auth", authRoute);
+app.use("/api/message", messageRoute);
 
 //!express server is listening
 app.listen(PORT, () => {
